1d28201ae7c802b3d0a083054a8c7712fcc3351a
Jean-Luc CHASSAING <jlchassaing@gmail.com>
2020-04-07 10:51:38 +0200

    fix: missing YamlFileLoader in extension file

--- src/bundle/DependencyInjection/EzGeoDataGouvExtension.php
+++ src/bundle/DependencyInjection/EzGeoDataGouvExtension.php
@@ -9,6 +9,8 @@ namespace eZGeoDataGouvBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
 
 class EzGeoDataGouvExtension extends Extension
 {
