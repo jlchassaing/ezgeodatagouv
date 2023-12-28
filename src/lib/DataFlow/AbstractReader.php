f8dba59a616d2a720f1784b233926f46e2404d59
Jean-Luc Chassaing <jlchassaing@gmail.com>
2023-12-28 10:47:14 +0100

    [Update] fix file exist if network

--- src/lib/DataFlow/AbstractReader.php
+++ src/lib/DataFlow/AbstractReader.php
@@ -6,16 +6,17 @@
 
 namespace eZGeoDataGouv\DataFlow;
 
-
 abstract class AbstractReader implements ReaderInterface
 {
     protected $config;
     protected $separator;
+    protected $enclosure;
 
     public function init($config): AbstractReader
     {
         $this->config = $config;
         $this->separator = $config['csv_field_separator'] ?? ';';
+        $this->enclosure = $config['enclosure'] ?? '';
         return $this;
     }
 
@@ -24,17 +25,37 @@ abstract class AbstractReader implements ReaderInterface
     public function fileExists(string $filename): bool
     {
         if (preg_match('/^(http[s]*|ftp):\/\//',$filename, $matches)){
-            $headers = @get_headers($filename);
-            $exists = false;
-            foreach ($headers as $header) {
-                if (preg_match('/HTTP\/1\.\d{1} 200 OK/', $header)){
-                    $exists = true;
-                    break;
-                }
+            
+            $ch = curl_init($filename);
+            
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+
+            // Execute
+            curl_exec($ch);
+            
+            $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            
+            // Check HTTP status code
+            if (!curl_errno($ch)) {
+                return curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200;
             }
-            return $exists;
+            
+            return false;
+            
         }
         return file_exists($filename);
+    }
+
+    public function initFieldSeparators(string $line): void
+    {
+        $this->separator = str_contains($line, ';')
+            ? ';'
+            : ',';
+        if (preg_match('/["\']*' . $this->separator . '["\']*/', $line, $match)) {
+            if (count($match) === 3 && $match[1] === $match[2]) {
+                $this->enclosure = $match[1];
+            }
+        }
     }
 
 
